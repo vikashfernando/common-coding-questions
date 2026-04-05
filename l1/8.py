4044b494834b3564717d4554a5eff28c74612d59
vikashfernando <vikashdeshitha@gmail.com>
2026-04-05 11:38:49 +0530

    l8 done in 2 ways and tested

--- l1/8.py
+++ l1/8.py
@@ -1,2 +1,26 @@
 #8. Reverse a number
 #Example: 123 → 321
+
+lst1=[]
+num="2026"
+
+for i in num:
+    lst1.append(i)
+
+lst2=lst1[-1::-1]
+
+for j in lst2:
+    print(j, end="")
+
+
+
+print(" ")
+print("*"*40)
+print("easiest way")
+
+
+
+num1=2026
+num1=str(num1)
+reversedNum=num1[-1::-1]
+print(int(reversedNum))
