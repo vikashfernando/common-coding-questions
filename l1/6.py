57674e1df58a2861d945c5b2765f209e39b4a8b0
vikashfernando <vikashdeshitha@gmail.com>
2026-04-05 10:56:27 +0530

    l6 done

--- l1/6.py
+++ l1/6.py
@@ -1,1 +1,6 @@
 #6. Print multiplication table of a number
+
+num=int(input("enter the number: "))
+
+for i in range(1,13):
+    print(str(i)+" * "+str(num)+" = "+str(i*num))
--- l1/7.py
+++ l1/7.py
@@ -1,2 +1,4 @@
 #7. Count digits in a number
 #Example: 1234 → 4
+
+
