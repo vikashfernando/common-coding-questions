e73fc7627ea35cdfb4669da7b145a276c1d348df
vikashfernando <vikashdeshitha@gmail.com>
2026-04-07 07:16:31 +0530

    added 2 activities

---
+++ foundFromNet/2.py
@@ -0,0 +1,9 @@
+#Find the maximum number in a list
+
+lst=[1,3,5,3,6,4,7]
+
+lst.sort()
+print(lst[-1])
+
+
+# append()	clear()	count()	index()	insert() pop() remove() reverse() sort()
---
+++ foundFromNet/3.py
@@ -0,0 +1,11 @@
+##Find the maximum number in a list
+
+lst=[3,4,5,6,1,15,6]
+
+number=0
+
+for i in lst:
+    if i>number:
+        number=i
+
+print(number)
